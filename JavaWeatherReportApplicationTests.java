542bd6abe2b3877ef2eaaec4f0cefad4e07e7bd3
Benita Joylin <152283811+benjoy27@users.noreply.github.com>
2024-04-12 08:41:30 +0530

    Add files via upload

---
+++ JavaWeatherReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaweatherreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeatherReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
